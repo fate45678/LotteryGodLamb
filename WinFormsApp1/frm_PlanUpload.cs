3b6a9b2766a1e6d9d830ea5e91992ce33bed2b58
DESKTOP-UNEFRJO\Flankao <fate45678@gmail.com>
2018-07-03 16:09:16 +0800

    combobox修正 問題很麻煩 切換時候處理的慢一點

--- WinFormsApp1/frm_PlanUpload.cs
+++ WinFormsApp1/frm_PlanUpload.cs
@@ -2811,7 +2811,7 @@ namespace WinFormsApp1
                     cbGameKind.Items.Add("中三");
                     cbGameKind.Items.Add("后三");
                 }
-                if (frm_PlanCycle.GameLotteryName == "天津时时彩")
+                else if (frm_PlanCycle.GameLotteryName == "天津时时彩")
                 {
                     filtercbItem(int.Parse(frmGameMain.globalGetCurrentPeriod.Substring(frmGameMain.globalGetCurrentPeriod.Length - 3, 3)));
                     label2.Text = "共" + 1 + "期";
@@ -2828,7 +2828,7 @@ namespace WinFormsApp1
                     //label2.Text = "共" + calPeriod() + "期";
                     //label23.Text = cbGamePlan.Text + "~" + cbGameCycle.Text + " 共" + calPeriod() + "期";
                 }
-                if (frm_PlanCycle.GameLotteryName == "新疆时时彩")
+                else if (frm_PlanCycle.GameLotteryName == "新疆时时彩")
                 {
                     filtercbItem(int.Parse(frmGameMain.globalGetCurrentPeriod.Substring(frmGameMain.globalGetCurrentPeriod.Length - 3, 3)));
                     label2.Text = "共" + 1 + "期";
@@ -2845,7 +2845,7 @@ namespace WinFormsApp1
                     //label2.Text = "共" + calPeriod() + "期";
                     //label23.Text = cbGamePlan.Text + "~" + cbGameCycle.Text + " 共" + calPeriod() + "期";
                 }
-                isChangeLotteryName = false;
+                
 
                 frm_LoadingControl frm_LoadingControl = new frm_LoadingControl();
                 frm_LoadingControl.Show();
@@ -2880,6 +2880,7 @@ namespace WinFormsApp1
                 cbGameKind.SelectedIndex = 0;
 
                 frm_LoadingControl.Close();
+                isChangeLotteryName = false;
                 updatecheckboxlist1(0);
 
             }
